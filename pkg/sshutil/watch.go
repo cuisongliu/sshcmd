1a78e7e48420ad93bc90946e6db8aebe94d029a0
cuisongliu <cuisongliu@qq.com>
2020-03-25 23:35:03 +0800

    fix(v1.0): support 实时输出

--- pkg/sshutil/watch.go
+++ pkg/sshutil/watch.go
@@ -40,7 +40,7 @@ func (ss *SSH) IsFilExist(host, remoteFilePath string) bool {
 	remoteFileDirName := path.Dir(remoteFilePath)
 	//it's bug: if file is aa.bak, `ls -l | grep aa | wc -l` is 1 ,should use `ll aa 2>/dev/null |wc -l`
 	//remoteFileCommand := fmt.Sprintf("ls -l %s| grep %s | grep -v grep |wc -l", remoteFileDirName, remoteFileName)
-	remoteFileCommand := fmt.Sprintf("ll %s/%s 2>/dev/null |wc -l", remoteFileDirName, remoteFileName)
+	remoteFileCommand := fmt.Sprintf("ls -l %s/%s 2>/dev/null |wc -l", remoteFileDirName, remoteFileName)
 
 	data := ss.CmdToString(host, remoteFileCommand, " ")
 	count, err := strconv.Atoi(strings.TrimSpace(data))
